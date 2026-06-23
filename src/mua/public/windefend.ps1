3af069ed872a4ddcd71586ef13692ebad01fa586
jesse kirk <jesse.kirk.1.ctr@us.af.mil>
2026-06-23 07:43:40 -0500

    fix space between parameters

--- src/mua/public/windefend.ps1
+++ src/mua/public/windefend.ps1
@@ -7,7 +7,7 @@
 $ProgressPreference = 'SilentlyContinue' ; $path = $destinationPath
 $uri = 'https://go.microsoft.com/fwlink/?LinkId=197094' # Network Real-time Inspection definitions | nis_full.exe
 $filename = 'nis_full.exe' ; Write-Verbose -Message "Downloading $filename..." -Verbose
-Start-BitsTransfer -Source $uri -Destination $(Join-Path -Path $path -ChildPath $filename)-TransferType Download ; Write-Host -Object "done! downloaded $filename to $path." -ForegroundColor Cyan
+Start-BitsTransfer -Source $uri -Destination $(Join-Path -Path $path -ChildPath $filename) -TransferType Download ; Write-Host -Object "done! downloaded $filename to $path." -ForegroundColor Cyan
 $uri = 'https://go.microsoft.com/fwlink/?LinkID=121721&arch=x64' # Definitions | mpam-fe.exe
 $filename = 'mpam-fe.exe' ; Write-Verbose -Message "Downloading $filename..." -Verbose
-Start-BitsTransfer -Source $uri -Destination $(Join-Path -Path $path -ChildPath $filename)-TransferType Download ; Write-Host -Object "done! downloaded $filename to $path." -ForegroundColor Cyan
+Start-BitsTransfer -Source $uri -Destination $(Join-Path -Path $path -ChildPath $filename) -TransferType Download ; Write-Host -Object "done! downloaded $filename to $path." -ForegroundColor Cyan
